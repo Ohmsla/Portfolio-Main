a22d422eea08bdb17bc0c89ef05ca4ab7e8cba95
Ohmsla <lawohms2525@gmail.com>
2025-03-17 11:38:03 +0530

    query

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    document.querySelector(".right").classList.toggle("active");
+  }
+  
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
